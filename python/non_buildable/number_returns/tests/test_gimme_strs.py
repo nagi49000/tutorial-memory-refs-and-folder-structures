58cbc83bb5440928b54d05eb2ed054df0ce7f964
nagi <nagi>
2022-03-26 20:59:49 +0000

    added another notebook

---
+++ python/non_buildable/number_returns/tests/test_gimme_strs.py
@@ -0,0 +1,5 @@
+from number_returns.gimme_strs import gimme5str
+
+def test_gimme5str():
+    assert gimme5str() == '5' 
+
---
+++ python/non_buildable_2/number_returns/tests/test_gimmes.py
@@ -0,0 +1,7 @@
+import number_returns.gimmes
+
+def test_gimme3():
+    assert number_returns.gimmes.gimme3() == 3
+
+def test_gimme5():
+    assert number_returns.gimmes.gimme5() == 5
